234dec9d1c11d4626cff3a0e4ffd228b7cb2417b
Nikasvirid <100194539+Nikasvirid@users.noreply.github.com>
2022-08-07 21:31:11 +0300

    fix webpack.confg.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-// const CopyPlugin = require("copy-webpack-plugin");//
+const CopyPlugin = require("copy-webpack-plugin");
 
 const baseConfig = {
   entry: path.resolve(__dirname, "./src/index.ts"),
