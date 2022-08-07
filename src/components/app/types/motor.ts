6598240a8eefdc939075b091c9f701d6988dd0c9
Nikasvirid <100194539+Nikasvirid@users.noreply.github.com>
2022-08-07 21:15:27 +0300

    add intarface

--- src/components/app/types/motor.ts
+++ src/components/app/types/motor.ts
@@ -2,3 +2,11 @@ export type MotorCar = {
     rapidity: number;
     distance: number;
   };
+  export enum STATUS {
+    OK = 200,
+    CREATED = 201,
+    NOT_FOUND = 404,
+    BAD_REQUEST = 400,
+    TOO_MANY_REQUESTS = 429,
+    INTERNAL_SERVER_ERROR = 500,
+}
