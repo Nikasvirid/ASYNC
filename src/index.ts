ce8e88012dcf9f220b3835cf06a6af00018f2375
Nikasvirid <100194539+Nikasvirid@users.noreply.github.com>
2022-08-10 15:13:41 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,40 @@
-import App from "./components/app/app";
-import "./global.scss";
+import { getId } from './utils/utils';
+import renderApp from './components/app/app';
+import { VIEW_NAME } from './constants';
+import createCar from './utils/createCar';
+import paginate from './utils/paginate';
+import generateCars from './utils/generateCars';
+import './index.scss';
+import deleteCar from './utils/deleteCar';
+import sorts from './utils/sorts';
+const root = document.querySelector('#root');
+root.innerHTML = renderApp();
 
-new App();
+const garageViewEl = document.querySelector('.garage-view') as HTMLElement;
+const winnersViewEl = document.querySelector('.winners-view') as HTMLElement;
+
+const createCarNameInput = document.querySelector('#create-car-name') as HTMLInputElement;
+const createCarColorInput = document.querySelector('#create-car-color') as HTMLInputElement;
+
+const updateCarNameInput = document.querySelector('#update-car-name') as HTMLInputElement;
+const updateCarColorInput = document.querySelector('#update-car-color') as HTMLInputElement;
+const updateSubmit = document.querySelector('#submit-update-car') as HTMLInputElement;
+
+const garagePaginationPrevBtn = document.querySelector('#garage-pagination-prev') as HTMLButtonElement;
+const garagePaginationNextBtn = document.querySelector('#garage-pagination-next') as HTMLButtonElement;
+const garageCurrentPageEl = document.querySelector('#garage-current-page-num') as HTMLElement;
+
+const winnersPaginationPrevBtn = document.querySelector('#winners-pagination-prev') as HTMLButtonElement;
+const winnersPaginationNextBtn = document.querySelector('#winners-pagination-next') as HTMLButtonElement;
+const winnersCurrentPageEl = document.querySelector('#winners-current-page-num') as HTMLElement;
+
+const carsEl = document.querySelector('#cars') as HTMLElement;
+const winnersEl = document.querySelector('#winners') as HTMLElement;
+
+const handlerOnClick = async (event: MouseEvent) => {
+    const { id } = event.target as HTMLElement;
+        
+};
+
+
+document.addEventListener('click', handlerOnClick);
